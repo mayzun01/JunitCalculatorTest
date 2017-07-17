4cc276e2de55560caed1ecce0dbc55a9aa059b6c
mayzun01 <mayzonmoeooyg@gmail.com>
2017-07-17 15:37:28 +0630

    Create AdditionTest.java

---
+++ src/AdditionTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class AdditionTest {
+	private int x = 10;
+	private int y = 5;
+	
+	Addition addition = new Addition(x,y);
+
+	@Test
+	public void testAddition()
+	{
+		System.out.println("*** Test use of Addition ***");
+		int z = addition.twoValues();
+		System.out.println(" Result: addition of " +x+ " and " +y+ " is " + z );
+		assertEquals(15,z);
+	}
+}
