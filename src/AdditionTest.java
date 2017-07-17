5d9a033edde7d34c74e7ce1e8271ac73d6fc786f
mayzun01 <mayzonmoeooyg@gmail.com>
2017-07-17 16:52:12 +0630

    Update AdditionTest.java

--- src/AdditionTest.java
+++ src/AdditionTest.java
@@ -14,6 +14,6 @@ public class AdditionTest {
 		System.out.println("*** Test use of Addition ***");
 		int z = addition.twoValues();
 		System.out.println(" Result: addition of " +x+ " and " +y+ " is " + z );
-		assertEquals(15,z);
+		assertEquals(20,z);
 	}
 }
